83e9e08c4820f4902b3d1c8732c29eacea10712f
Herin Zaveri <herin.zaveri2@gmail.com>
2023-07-14 14:01:45 +0530

    request-cert: vendor deps, migrate to latest K8S client

---
+++ request-cert/go.mod
@@ -0,0 +1,22 @@
+module github.com/k8s/request-cert
+
+go 1.12
+
+require (
+	github.com/PuerkitoBio/purell v1.0.0 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20160726150825-5bd2802263f2 // indirect
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/elazarl/goproxy v0.0.0-20170405201442-c4fc26588b6e // indirect
+	github.com/emicklei/go-restful v0.0.0-20170410110728-ff4f55a20633 // indirect
+	github.com/ghodss/yaml v0.0.0-20150909031657-73d445a93680 // indirect
+	github.com/go-openapi/spec v0.0.0-20160808142527-6aced65f8501 // indirect
+	github.com/googleapis/gnostic v0.0.0-20170729233727-0c5108395e2d // indirect
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/pkg/errors v0.9.1
+	golang.org/x/tools v0.7.0
+	k8s.io/api v0.27.3
+	k8s.io/apimachinery v0.27.3
+	k8s.io/client-go v0.27.3
+	k8s.io/klog v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff v0.0.0-20190525122527-15d366b2352e // indirect
+)
--- request-cert/k8s_certificates.go
+++ request-cert/k8s_certificates.go
@@ -22,7 +22,9 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	certificates "k8s.io/api/certificates/v1beta1"
+
+	certificates "k8s.io/api/certificates/v1"
+	context "context"
 	core "k8s.io/api/core/v1"
 	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
 	types "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -82,22 +84,28 @@ func getKubernetesCertificate(csrName string, csr []byte, wantServerAuth bool, a
 	}
 
 	// Build the certificate signing request.
+	ctx := context.TODO()
 	req := &certificates.CertificateSigningRequest{
 		TypeMeta:   types.TypeMeta{Kind: "CertificateSigningRequest"},
 		ObjectMeta: types.ObjectMeta{Name: csrName},
 		Spec: certificates.CertificateSigningRequestSpec{
 			Request: csr,
 			Usages:  keyUsages,
+			SignerName: "kubernetes.io/kube-apiserver-client",
 		},
+	}
+	createOptions := types.CreateOptions{
+		TypeMeta:   types.TypeMeta{Kind: "CertificateSigningRequest"},
 	}
 
 	fmt.Printf("Sending create request: %s for %s\n", req.Name, *addresses)
-	resp, err := client.Certificates().CertificateSigningRequests().Create(req)
+	resp, err := client.CertificatesV1().CertificateSigningRequests().Create(ctx, req, createOptions)
 
 	if err != nil && k8s_errors.IsAlreadyExists(err) && allowPrevious {
 		fmt.Printf("Attempting to use previous CSR: %s\n", req.Name)
+		ctx := context.TODO()
 		getOpts := types.GetOptions{TypeMeta: types.TypeMeta{Kind: "CertificateSigningRequest"}}
-		resp, err = client.Certificates().CertificateSigningRequests().Get(req.Name, getOpts)
+		resp, err = client.CertificatesV1().CertificateSigningRequests().Get(ctx, req.Name, getOpts)
 	}
 	if err != nil {
 		return nil, errors.Wrapf(err, "CertificateSigningRequest.Create(%s) failed", req.Name)
@@ -113,7 +121,7 @@ func getKubernetesCertificate(csrName string, csr []byte, wantServerAuth bool, a
 		FieldSelector:  fields.OneTermEqualSelector("metadata.name", csrName).String(),
 	}
 
-	resultCh, err := client.Certificates().CertificateSigningRequests().Watch(watchReq)
+	resultCh, err := client.CertificatesV1().CertificateSigningRequests().Watch(ctx, watchReq)
 	if err != nil {
 		return nil, errors.Wrapf(err, "CertificateSigningRequest.Watch(%s) failed: %v", csrName)
 	}
@@ -171,14 +179,16 @@ func storeSecrets(secretName string, cert []byte, key []byte) error {
 		return err
 	}
 
+	ctx := context.TODO()
 	secret := &core.Secret{
 		ObjectMeta: types.ObjectMeta{
 			Name: secretName,
 		},
 		Data: map[string][]byte{"cert": cert, "key": key},
 	}
+	createOptions := types.CreateOptions{}
 
-	_, err = client.CoreV1().Secrets(*namespace).Create(secret)
+	_, err = client.CoreV1().Secrets(*namespace).Create(ctx, secret, createOptions)
 	return err
 }
 
@@ -190,7 +200,9 @@ func getSecrets(secretName string) ([]byte, []byte, error) {
 		return nil, nil, err
 	}
 
-	secret, err := client.Core().Secrets(*namespace).Get(secretName, types.GetOptions{})
+	ctx := context.TODO()
+
+	secret, err := client.CoreV1().Secrets(*namespace).Get(ctx, secretName, types.GetOptions{})
 	if err != nil {
 		if k8s_errors.IsNotFound(err) {
 			return nil, nil, nil
